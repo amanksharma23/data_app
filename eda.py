87a7cfc746ffabf857e0b625c78669bb4a90e41c
Aman Kumar Sharma <101040201+amanksharma23@users.noreply.github.com>
2023-03-22 18:43:58 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from edafunc import preprocessing
-
+from edafunc import pca_maker
 
 st.set_page_config(layout="wide")
 row0_1,row0_2,name_spc = st.columns((3, 1,1))
@@ -26,15 +26,10 @@ with row_space:
     def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
         return df.to_csv().encode('utf-8')
-    sample_data = pd.read_csv('app_category_dataset.csv')
-    csv = convert_df(sample_data)
-    row_space.download_button(
-        label="Download sample data",
-        data=csv,
-        file_name='app_category_dataset.csv',
-        mime='text/csv',
-        )
- 
+    
+def load_data():
+            df = pd.read_csv('app_category_dataset.csv')
+            return df
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -43,6 +38,7 @@ if uploaded_file is not None:
         time.sleep(1)
     
     metrics, dimensions, dates, category = preprocessing(df)
+    pca_data, pca_cols = pca_maker(df)
 
     
     bar1,bar2,pie1,pie2 = st.columns((1.5,1.5,1,1))
@@ -58,18 +54,88 @@ if uploaded_file is not None:
     with pie2:
         metric_piecol = pie2.selectbox("Select the values", options= metrics)
 
-    trend,row_spc1 = st.columns((3,2))
+    trend,row_spc1,row_spc2 = st.columns((2.5,1.5,1))
     with trend:
         st.subheader("Trend :blue[Analysis]")
     trend1,trend2 = st.columns((1.5,1.5))
     
     with row_spc1:
-        metric_trend1 =  row_spc1.multiselect("Select the metric for trend analysis", options= metrics)
+        date_trend2 =  st.selectbox("Select the date column for trend analysis", options= dates)
+    with row_spc2:
+        metric_trend1 =  st.multiselect("Select the metric for trend analysis", options= metrics)
 
     trend2,row_spc21 = st.columns((4.9,.1))
 
     bar.plotly_chart(px.histogram(data_frame=df,x=categorical_col, y = metric_col,template='plotly', height = 500,text_auto = True), use_container_width=True)
     pie.plotly_chart(px.pie(data_frame=df,values=metric_piecol,names=categorical_piecol, height=500))
-    trend2.plotly_chart(px.line(data_frame=df,x=df['Date'], y = metric_trend1, template='plotly', width = 1200,height = 500), use_container_width=False)
+    trend2.plotly_chart(px.line(data_frame=df,x=date_trend2, y = metric_trend1, template='plotly', width = 1200,height = 500), use_container_width=False)
+
+    pca, pca_rs = st.columns((3.5,1.5))
+    with pca:
+        st.subheader("Principle Component :blue[Analysis]")
+    with pca_rs:
+        st.title("")
+        st.title("")
+        cat_col = pca_rs.selectbox("Select Dimensions", options= category)
+        pca_1 = pca_rs.selectbox("First Principle Component", options=pca_cols, index=0)
+        pca_2 = pca_rs.selectbox("Second Principle Component", options=pca_cols)
+            
+        pca.plotly_chart(px.scatter(data_frame=pca_data, x=pca_1, y=pca_2,color=cat_col, template="plotly", height=500, width=500), use_container_width=True)
+    
+
+
 else:
     row0_1.subheader("Please upload a .csv file")
+    if row_space.checkbox('Check to use Example Dataset'):
+        
+        df = load_data()
+
+        with st.spinner('Wait for it...'):
+            time.sleep(1)
+        
+            metrics, dimensions, dates, category = preprocessing(df)
+            pca_data,pca_cols = pca_maker(df)
+
+        
+        bar1,bar2,pie1,pie2 = st.columns((1.5,1.5,1,1))
+        bar,pie = st.columns((3,2))
+
+        with bar1:
+            categorical_col = bar1.selectbox("Select the dimension", options= category)
+        with bar2:
+            metric_col = bar2.selectbox("Select the metric", options= metrics)
+
+        with pie1:
+            categorical_piecol = pie1.selectbox("Select the names", options= category)
+        with pie2:
+            metric_piecol = pie2.selectbox("Select the values", options= metrics)
+
+        trend,row_spc1,row_spc2 = st.columns((2.5,1.5,1))
+        with trend:
+            st.subheader("Trend :blue[Analysis]")
+        trend1,trend2 = st.columns((1.5,1.5))
+        
+        with row_spc1:
+            date_trend2 =  st.selectbox("Select the date column for trend analysis", options= dates)
+        with row_spc2:
+            metric_trend1 =  st.multiselect("Select the metric for trend analysis", options= metrics)
+
+        trend2,row_spc21 = st.columns((4.9,.1))
+
+        bar.plotly_chart(px.histogram(data_frame=df,x=categorical_col, y = metric_col,template='plotly', height = 500,text_auto = True), use_container_width=True)
+        pie.plotly_chart(px.pie(data_frame=df,values=metric_piecol,names=categorical_piecol, height=500))
+        trend2.plotly_chart(px.line(data_frame=df,x=date_trend2, y = metric_trend1, template='plotly', width = 1200,height = 500), use_container_width=False)
+
+        
+        pca, pca_rs = st.columns((3.5,1.5))
+        with pca:
+            st.subheader("Principle Component :blue[Analysis]")
+        with pca_rs:
+            st.title("")
+            st.title("")
+            cat_col = pca_rs.selectbox("Select Dimensions", options= category)
+            pca_1 = pca_rs.selectbox("First Principle Component", options=pca_cols, index=0)
+            pca_2 = pca_rs.selectbox("Second Principle Component", options=pca_cols)
+            
+        pca.plotly_chart(px.scatter(data_frame=pca_data, x=pca_1, y=pca_2, color=cat_col, template="simple_white", height=500, width=500), use_container_width=True)
+    
